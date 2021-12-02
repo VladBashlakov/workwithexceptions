f006df05558cf3c6c5932304ef5fb2e102a0e6cb
Vlad <VladBashlakov@gmail.com>
2021-12-02 21:45:35 +0300

    fc

---
+++ com/example/workwithexceptions/WorkwithexceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.workwithexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkwithexceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkwithexceptionsApplication.class, args);
+    }
+
+}
